28493efa1f8d53109a8c44e881ccb0253f14171b
matPacheco <matheus.pacheco17@outlook.com>
2025-02-25 10:33:28 -0300

    testando ambiente customizado

--- tutoriais/basic_custom_env.py
+++ tutoriais/basic_custom_env.py
@@ -1,27 +1,44 @@
+import gym_pybullet_drones
 import gymnasium as gym
+import random
+import numpy as np
 
-
-env = gym.make("GPS-distance-v0", render_mode="human")
+initial_rpys=np.array([[0.0, 0.0, 1.0]])
+env = gym.make("GPS-Distance-v0", rng=random.Random(42), initial_rpys=initial_rpys)
+print(type(env))
 print("Action Space:")
 print(env.action_space)  # Discrete(4) <class 'gymnasium.spaces.discrete.Discrete'>
 print("Observation Space:")
 print(env.observation_space)
 observation, info = env.reset(seed=42)
+terminated = False
+i = 0
+action = np.array([[0., 0., 0., 0.]])
+observation, reward, terminated, truncated, info = env.step(action)
+print("Action:", action)
+print("Step   0, Observation:", observation[0][0][0])
 
-for _ in range(10):
-   action = env.action_space.sample()  # this is where you would insert your policy
-   print("Action", action)
+for _ in range(480):
+   i += 1
+   # action = env.action_space.sample()  # this is where you would insert your policy
+   # print("Action", action)
    observation, reward, terminated, truncated, info = env.step(action)
+   state = env._getDroneStateVector()
+   print(state)
+   if i == 240:
+      print("Step 240, Velocidade:", observation[0][0][1])
+   # print("Observation:", observation[0][0][0])
+   # print(observation)
+   # print("Reward:", reward)
+   # print("Terminated:", terminated)
+   # print("Truncated:", truncated)
+   # print("Info:", info)
 
-   print("Observation:", observation)
-   print("Reward:", reward)
-   print("Terminated:", terminated)
-   print("Truncated:", truncated)
-   print("Info:", info)
-
-   print("------------")
+   # print("------------")
 
    if terminated or truncated:
       observation, info = env.reset()
+print("Step 480 Observation:", observation[0][0][0])
 
+print(i)
 env.close()
